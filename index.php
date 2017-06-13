bbaed7b683fd8e3a433888cc032e4406557acf79
ytteroy <mail262626@inbox.lv>
2017-06-13 16:27:06 +0300

    dropdown
    $c['page']['dropdown']

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	
 	<title><?php echo $c['page']['title']; ?></title>
 	<link rel="stylesheet" type="text/css" href="//cdn.airtel.lv/beagle/css/style.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo $c['url'] . '/' . $c['page']['directory']; ?>/assets/css/baltgro.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $c['url'] . '/' . $c['page']['directory']; ?>/assets/css/baltgro.css?<?php echo time(); ?>">
 </head>
 <body class="baltsms">
 	<div class="col-md-<?php echo $c['page']['size'][0]; ?> col-md-offset-<?php echo $c['page']['size'][1]; ?>">
@@ -18,18 +18,40 @@
 			</div>
 		</div>
 		<ul id="tab-nav" class="nav nav-tabs" role="tablist">
-			<?php if($c['page']['language'] === true): ?>
+			<?php
+			if($c['page']['language'] === true){
+			?>
 				<div id="baltsms-flags">
-					<?php foreach($c['lang'] as $language_key => $data): ?>
+					<?php foreach($c['lang'] as $language_key => $data){ ?>
 						<img src="<?php echo $c['url'] . '/' . $c['page']['directory']; ?>/assets/images/flags/<?php echo $language_key; ?>.gif" onClick="setLanguage('<?php echo $language_key; ?>')">
-					<?php endforeach; ?>
+					<?php } ?>
 				</div>
-			<?php endif; ?>
-			<?php foreach($c['sms']['plugins'] as $type => $plugins): ?>
-				<?php foreach($plugins as $index => $plugin): ?>
+			<?php
+			}
+			
+			if($c['page']['dropdown'] == true){
+				foreach($c['sms']['plugins'] as $type => $plugins){
+				?>
+				<li role="presentation" class="dropdown <?php echo ($c['sms']['primary'] == $type) ? "active" : ""; ?>">
+					<a href="#" id="plugins-<?php echo $type; ?>" class="dropdown-toggle" data-toggle="dropdown" aria-controls="plugins-<?php echo $type; ?>-contents"><?php echo (isset($lang['plugin-type-' . $type])) ? $lang['plugin-type-' . $type] : "<span style='color: red'>language not found</span>"; ?> <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu" aria-labelledby="plugins-<?php echo $type; ?>" id="plugins-<?php echo $type; ?>-contents">
+						<?php foreach($plugins as $index => $plugin): ?>
+							<li role="presentation" class="<?php echo ($c['sms']['primary'] == $type AND $index == 0) ? "active" : ""; ?>" onClick="loadPlugin('<?php echo $plugin; ?>')"><a href="#<?php echo $plugin; ?>" aria-controls="<?php echo $plugin; ?>" role="tab" style="color:black; border-bottom:initial;" data-toggle="tab"><?php echo (isset($lang['plugin-' . $plugin])) ? $lang['plugin-' . $plugin] : "<span style='color: red'>language not found</span>"; ?></a></li>
+						<?php endforeach; ?>
+					</ul>
+				</li>
+				<?php
+				}
+			}else{
+				foreach($c['sms']['plugins'] as $type => $plugins){
+					foreach($plugins as $index => $plugin){
+					?>
 					<li role="presentation" class="<?php echo ($c['sms']['primary'] == $type AND $index == 0) ? "active" : ""; ?>" onClick="loadPlugin('<?php echo $plugin; ?>')"><a href="#<?php echo $plugin; ?>" aria-controls="<?php echo $plugin; ?>" role="tab" data-toggle="tab"><?php echo (isset($lang['plugin-' . $plugin])) ? $lang['plugin-' . $plugin] : "<span style='color: red'>language not found</span>"; ?></a></li>
-				<?php endforeach; ?>
-			<?php endforeach; ?>
+					<?php
+					}
+				}
+			}
+			?>
 		</ul>
 		<div id="baltsms-page">
 			<div id="baltsms-content">
